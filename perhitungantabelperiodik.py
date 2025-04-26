455f7876dcd1b8ed7e6e73cc6344ea6d8a58dd4c
kelompok3pli <hakimluki31@gmail.com>
2025-04-26 09:10:30 +0700

    perhitungantabelperiodik.py

--- perhitungantabelperiodik.py
+++ perhitungantabelperiodik.py
@@ -4,3 +4,26 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+[
+    {
+        "name": "Hydrogen",
+        "symbol": "H",
+        "atomic_number": 1,
+        "atomic_mass": 1.008,
+        "category": "Nonmetal",
+        "density": 0.00008988,
+        "melting_point": 14.01,
+        "boiling_point": 20.28
+    },
+    {
+        "name": "Helium",
+        "symbol": "He",
+        "atomic_number": 2,
+        "atomic_mass": 4.0026,
+        "category": "Noble Gas",
+        "density": 0.0001786,
+        "melting_point": 0.0,
+        "boiling_point": 4.22
+    },
+    ...
+]
